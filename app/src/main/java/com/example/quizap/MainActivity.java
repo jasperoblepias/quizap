8d67668b5eb4cb09081e178ad9899b9fa0846399
jasperlance <jasperlance.oblepias.iics@ust.edu.ph>
2020-10-08 00:11:53 +0800

    Change to android api 26

---
+++ app/src/main/java/com/example/quizap/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.quizap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+}
